829ea85252fd09a67ef9ecbd4990115e3c7fa20d
mjp2220 <mjp2220@columbia.edu>
2015-12-01 11:29:20 -0500

    Initial commit

---
+++ Density/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Density
+//
+//  Created by Matt on 12/1/15.
+//  Copyright © 2015 Matthew Piccolella. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let DensityKey: String = "XK1XP46MGUAQJSEEMKYWNOISXKAI537A"
---
+++ Podfile
@@ -0,0 +1,15 @@
+platform :ios, '8.0'
+use_frameworks!
+
+target 'Density' do
+  pod 'Alamofire', '~> 3.0'
+end
+
+target 'DensityTests' do
+
+end
+
+target 'DensityUITests' do
+
+end
+
